0529c9d95070d53fe5699839005692fa083647fb
eternalpaininside <Bob.gubka@icloud.com>
2026-09-08 23:11:35 +0500

    level index v1 complete

--- internal/features/index/level/index.go
+++ internal/features/index/level/index.go
@@ -1,6 +1,10 @@
 package level
 
-import "fishyAHP/LogParser.git/internal/core/domain"
+import (
+	"sync"
+
+	"fishyAHP/LogParser.git/internal/core/domain"
+)
 
 type LogLevel string
 
@@ -14,4 +18,40 @@ const (
 
 type Index struct {
 	index map[LogLevel][]domain.RecordData
+	mtx   sync.RWMutex
+}
+
+func New() *Index {
+	return &Index{
+		index: make(map[LogLevel][]domain.RecordData, 5),
+		mtx:   sync.RWMutex{},
+	}
+}
+
+func (i *Index) Len() int {
+	i.mtx.RLock()
+	defer i.mtx.RUnlock()
+
+	return len(i.index)
+}
+
+func (i *Index) Add(comp LogLevel, data domain.RecordData) {
+	i.mtx.Lock()
+	defer i.mtx.Unlock()
+
+	i.index[comp] = append(i.index[comp], data)
+}
+
+func (i *Index) GetAll() map[LogLevel][]domain.RecordData {
+	i.mtx.RLock()
+	defer i.mtx.RUnlock()
+
+	return i.index
+}
+
+func (i *Index) Get(comp LogLevel) []domain.RecordData {
+	i.mtx.RLock()
+	defer i.mtx.RUnlock()
+
+	return i.index[comp]
 }
