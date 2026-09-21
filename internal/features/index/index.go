e379aad8537568e2b15ba30abda675cd0c2165f1
eternalpaininside <Bob.gubka@icloud.com>
2026-09-21 16:07:43 +0500

    complete timeindex interface

--- internal/features/index/index.go
+++ internal/features/index/index.go
@@ -7,14 +7,22 @@ import (
 )
 
 type Index[K comparable] interface {
-	Add(key K, value domain.RecordData)
-	Contains(key K) ([]domain.RecordData, bool)
-	Remove(key K) bool
+	Add(K, domain.RecordData)
+	Contains(K) ([]domain.RecordData, bool)
+	Remove(K) bool
 	Len() int
 	Clear()
 }
 
 type TimeIndex interface {
 	Add(time.Time, domain.RecordData) error
-	Min()
+	Find(time.Time)
+	Range(time.Time, time.Time)
+	Remove(time.Time)
+
+	Min() []domain.RecordData
+	Max() []domain.RecordData
+
+	Len()
+	Clear()
 }
