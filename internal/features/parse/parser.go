52d5cb6585bb7273a2a6429e4097f6881644d7c2
Kirill Kalinin <kinikalinin@edu.hse.ru>
2026-09-23 12:15:49 -0700

    add parser structure

--- internal/features/parse/parser.go
+++ internal/features/parse/parser.go
@@ -2,11 +2,21 @@ package features_parse
 
 import (
 	"fishyAHP/LogParser.git/internal/core/domain"
+	"fishyAHP/LogParser.git/internal/core/scheme"
 	"fishyAHP/LogParser.git/internal/features/tokenizer"
 )
+
+type LogParser struct {
+	scheme scheme.Scheme
+}
+
+func NewParser(s scheme.Scheme) LogParser {
+	return LogParser{
+		scheme: s,
+	}
+}
 
 // Parser представляет собой интерфейс, который позволяет с помощью метода Parse
 // превратить слайс байт в логическое представление лога и ошибку при невалидном входном слайсе
 type Parser interface {
 	Parse(data []tokenizer.Token) (domain.LogEntry, error)
-}
